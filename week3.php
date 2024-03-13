830e17709f5bd5ad3c6f078b58031b5c9608cef9
Markkkkkkkkkkkkkkkkkkkkkk <125893147+Markkkkkkkkkkkkkkkkkkkkkk@users.noreply.github.com>
2024-03-13 17:42:26 +0800

    Add files via upload

---
+++ week3.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<p><center><h1>張梓閎</h1>
+<img src="Mark.png" width="200" height="300" alt="照片"/></center></p>
+<hr color="black"width="100%" align ="left"/>
+<p><h2>簡介</h2>
+<ul>
+    <li><b>生日：</b>2003/11/22
+    <li><b>電話：</b>0975901492</li>
+    <li><b>信箱：</b>mark20031122@gmail.com</li>
+    <li><b>Instagram：</b><a href="https://www.instagram.com/mark20031122/">mark20031122</a></li>
+
+</ul></p>
+<hr color="black"width="100%" align ="left"/>
+<p><h2>自我介紹</h2>
+
+<font color="blue">您好，我是就讀於高雄大學的一名學生。我是一位樂觀、謙虛的人，並以斯文溫暖的態度對待生活和他人。我相信積極的心態能夠克服各種困難，並且樂於學習和成長。我喜歡與人交流，並且願意傾聽他人的意見和建議。在大學生活中，我希望能夠不斷挑戰自我，拓展自己的知識和技能，為未來的發展打下堅實的基礎。</font></p>
+<p><center><table border="">
+<thread>
+<tr>
+    <th>休閒活動</th>
+    <th>個性</th>
+    <th>興趣</th>
+</tr>
+</thread>
+<tr>
+    <td>打籃球、睡覺</td>
+    <td>文靜、善良、老實</td>
+    <td>看書、聽音樂</td>
+</tr>
+</table></center></p>
+<hr color="black"width="100%" align ="left"/>
+</body>
+</html>
